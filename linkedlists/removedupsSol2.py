5be1857137a450c12bf79c72c4d61bc0627d1262
jbgraille@gmail.com <jbgraille@gmail.com>
2020-07-02 22:47:13 +0200

    clever method

---
+++ linkedlists/removedupsSol2.py
@@ -0,0 +1,45 @@
+
+class Node():
+  """
+  Credits: w-hat ctci-solutions/ch-02-linked-lists/01-remove-duplicates.py
+  """
+
+  def __init__(self, data, following):
+    self.data = data
+    self.following = following
+
+
+def display(l):
+  print(l.data)
+  while l.following is not None:
+    l.data = l.following.data
+    print(l.data)
+    l.following = l.following.following
+
+
+def removedups(mylinkedlist):
+  l = mylinkedlist
+  ll = [l.data]
+  while l.following is not None:
+    if l.following.data in ll:
+      if l.following.following is not None:
+        l.following = l.following.following
+      else:
+        l.following = None
+    else:
+      ll.append(l.following.data)
+      l = l.following
+  return l
+
+
+def main():
+  mylinkedlist = Node(1,Node(2,Node(2,Node(4,None))))
+  #display(mylinkedlist)
+  res = removedups(mylinkedlist)
+  display(res)
+
+if __name__=='__main__':
+  main()
+
+
+
