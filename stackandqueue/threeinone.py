88959ce04ba80163824d19d78de9186122e2e91e
jbgraille@gmail.com <jbgraille@gmail.com>
2020-07-11 00:07:00 +0200

    fixing

--- stackandqueue/threeinone.py
+++ stackandqueue/threeinone.py
@@ -4,23 +4,20 @@ class ThreeStacks:
   def __init__(self,array,x,y,z):
     self.array = array
     self.len_array = len(array)
-    self.x = x
-    self.y = y
-    self.z = z
-    self.sizestack = [self.x,self.y,self.z] 
+    self.sizestack = [x,y,z] 
 
   @property
   def sizestack(self):
     return self._sizestack
   @sizestack.setter
-  def sizestack(self):
-    if sum([self.x,self.y,self.z]) != self.len_array:
+  def sizestack(self,sizestack):
+    if sum(sizestack) != self.len_array:
       raise TypeError("Sizes values can't match the initial size")
-    self._sizestack = [self.x,self.y,self.z]
+    self._sizestack = sizestack
 
 
 def main():
-  stacks = ThreeStacks([1,2,3,10,11,12,13],2,3,4)
+  stacks = ThreeStacks([1,2,3,10,11,12,13],2,1,4)
 
 
 if __name__ == '__main__':
