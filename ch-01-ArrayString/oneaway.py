55832f064eb0b8b0fb7ca8c19e3363f501dabce6
jbgraille@gmail.com <jbgraille@gmail.com>
2020-08-18 21:46:50 +0200

    tests cases

--- ch-01-ArrayString/oneaway.py
+++ ch-01-ArrayString/oneaway.py
@@ -1,5 +1,6 @@
 import difflib
 import re
+import unittest
 
 def find(val):
   res = re.search(r"(-|\+)", val)
@@ -13,11 +14,46 @@ def OneAway(string1,string2):
   resMatch = list(filter(lambda x: x != None, list(map(find,list(d.compare(string1,string2))))))
   dic = {'-':resMatch.count('-'), '+':resMatch.count('+')}
   print(dic)
-  if abs(dic['-'] - dic['+']) > 1 or (dic['-'] == dic['+'] and dic['-'] > 1):
+  if abs(dic['-'] - dic['+']) > 1 or (dic['-'] == dic['+'] and dic['-'] > 1) or (abs(dic['-'] - dic['+']) == 1 and dic['-'] != 0 and dic['+'] != 0):
     return False
   else:
     return True
+
+# https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/Chapter1/5_One%20Away/OneAway.py
+class Test(unittest.TestCase):
+    '''Test Cases'''
+    data = [
+        ('pale', 'ple', True),
+        ('pales', 'pale', True),
+        ('pale', 'bale', True),
+        ('paleabc', 'pleabc', True),
+        ('pale', 'ble', False),
+        ('a', 'b', True),
+        ('', 'd', True),
+        ('d', 'de', True),
+        ('pale', 'pale', True),
+        ('pale', 'ple', True),
+        ('ple', 'pale', True),
+        ('pale', 'bale', True),
+        ('pale', 'bake', False),
+        ('pale', 'pse', False),
+        ('ples', 'pales', True),
+        ('pale', 'pas', False),
+        ('pas', 'pale', False),
+        ('pale', 'pkle', True),
+        ('pkle', 'pable', False),
+        ('pal', 'palks', False),
+        ('palks', 'pal', False)
+    ]
+
+    def test_one_away(self):
+        for [test_s1, test_s2, expected] in self.data:
+            actual = OneAway(test_s1, test_s2)
+            self.assertEqual(actual, expected)
 def main():
+  print(OneAway('pale', 'pkle'))
+  unittest.main()
+  '''
   a = 'doretdeplatine'
   # remove case
   deletionOne = 'doretdeplatin'
@@ -35,6 +71,7 @@ def main():
   print(a,replaceOne,'->',OneAway(a,replaceOne))
   print(a,replaceSeveral,'->',OneAway(a,replaceSeveral))
   print(a,a,'->',OneAway(a,a))
+  '''
 
 if __name__ == '__main__':
   main()
