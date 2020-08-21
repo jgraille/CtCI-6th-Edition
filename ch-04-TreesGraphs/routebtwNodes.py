78dc0077491fb740722917cb848a7c348f040019
jbgraille@gmail.com <jbgraille@gmail.com>
2020-08-21 22:34:12 +0200

    have to deal with the queue

--- ch-04-TreesGraphs/routebtwNodes.py
+++ ch-04-TreesGraphs/routebtwNodes.py
@@ -2,6 +2,18 @@ class Node:
   def __init__(self,n,adjacent=None):
     self.n = n
     self.adjacent = adjacent
+    self._marked = False
+  
+  @property
+  def marked(self):
+    return self._marked
+
+  @marked.setter
+  def marked(self,value):
+    if isinstance(value,bool):
+      self._marked = value
+    else:
+      raise TypeError("Value is not a boolean")
 
 class Queue:
   def __init__(self,listQueue=None):
@@ -11,12 +23,24 @@ class Queue:
     return self.listQueue
   def removeElement(self):
     if self.listQueue:
-      self.listQueue.pop(0)
-    return self.listQueue
+      return self.listQueue.pop(0)
 
-    
+def routebtwNodes(Nodestart,end):
+  thequeue = Queue([])
+  Nodestart.marked = True
+  thequeue.addElement(Nodestart)
+  if thequeue.listQueue:
+    print(thequeue.listQueue)
+  else:
+    print('empty')
+    print(thequeue.listQueue)
+  while thequeue.listQueue:
+    node_r = thequeue.removeElement()
+    for n in node_r.adjacent:
+      if n.marked == False:
+        n.marked = True
+        thequeue.addElement(n)
 
-                                                                                                                                                    
 def main():
   G = { "a" : ["d"],
         "b" : ["c"],
@@ -33,10 +57,9 @@ def main():
         "E" : ["F","A"],
         "D" : ["A"]
         }
-queue = Queue([])
-queue.removeElement()
-queue.addElement(4)
-print(queue.listQueue)
+  node_f = Node("F",[Node("I")])
+  node_e = Node("E",[Node("F"),Node("A")])
+  routebtwNodes(node_e,"I")
 
 if __name__ == '__main__':
   main()
