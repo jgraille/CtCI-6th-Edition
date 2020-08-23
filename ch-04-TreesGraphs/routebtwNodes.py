0fb3bfd8a6e31135a7b8ceb36d3065619b89bfe4
jbgraille@gmail.com <jbgraille@gmail.com>
2020-08-23 16:16:05 +0200

    routes between nodes

--- ch-04-TreesGraphs/routebtwNodes.py
+++ ch-04-TreesGraphs/routebtwNodes.py
@@ -1,6 +1,8 @@
+import unittest
+
 class Node:
-  def __init__(self,n,adjacent=None):
-    self.n = n
+  def __init__(self,val,adjacent=None):
+    self.val = val
     self.adjacent = adjacent
     self._marked = False
   
@@ -15,6 +17,9 @@ class Node:
     else:
       raise TypeError("Value is not a boolean")
 
+  def addEdge(self,node):
+    self.adjacent += [node]
+
 class Queue:
   def __init__(self,listQueue=None):
     self.listQueue = listQueue
@@ -25,41 +30,50 @@ class Queue:
     if self.listQueue:
       return self.listQueue.pop(0)
 
-def routebtwNodes(Nodestart,end):
+def routebtwNodes(start,end):
+  if start == end:
+    return True
   thequeue = Queue([])
-  Nodestart.marked = True
-  thequeue.addElement(Nodestart)
-  if thequeue.listQueue:
-    print(thequeue.listQueue)
-  else:
-    print('empty')
-    print(thequeue.listQueue)
+  node_s = start
+  node_s.marked = True
+  thequeue.addElement(node_s)
   while thequeue.listQueue:
     node_r = thequeue.removeElement()
-    for n in node_r.adjacent:
-      if n.marked == False:
-        n.marked = True
-        thequeue.addElement(n)
+    if node_r.adjacent != None:
+      for r in node_r.adjacent:
+        if r.marked == False:
+          if r.val == end.val:
+            return True
+          else:
+            thequeue.addElement(r)
+          r.marked = True
+  return False
 
-def main():
-  G = { "a" : ["d"],
-        "b" : ["c"],
-        "c" : ["b", "c", "d", "e"],
-        "d" : ["a", "c"],
-        "e" : ["c"],
-        "f" : []
-      }
-  g = { "F" : ["I"],
-        "B" : ["J"],
-        "G" : ["D","H"],
-        "C" : ["G"],
-        "A" : ["B","C","D"],
-        "E" : ["F","A"],
-        "D" : ["A"]
-        }
-  node_f = Node("F",[Node("I")])
-  node_e = Node("E",[Node("F"),Node("A")])
-  routebtwNodes(node_e,"I")
+def resetMarked(nodes):
+  for i in nodes:
+    i.marked = False
+
+class Test(unittest.TestCase):
+  
+  def test_routebtwNodes(self):
+    node_c = Node('C')
+    node_d = Node('D')
+    node_b = Node('B', [node_c])
+    node_a = Node('A',[node_d,node_b])
+    node_e = Node('E',[node_a])
+    node_b.addEdge(node_a)
+    nodes = [node_a,node_b,node_c,node_d,node_e]
+    testCases = [[node_e,node_e,True],
+                 [node_a,node_b,True],
+                 [node_b,node_a,True],
+                 [node_e,node_c,True],
+                 [node_d,node_c,False]
+                 ]
+    for case in testCases:
+      print('(node_' + case[0].val + ',node_' + case[1].val + ')' + ' ' + str(case[2]))
+      self.assertEqual(routebtwNodes(case[0],case[1]),case[2])
+      resetMarked(nodes)
+  
 
 if __name__ == '__main__':
-  main()
+  unittest.main()
