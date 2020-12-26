9e4619b16eb24f93fe61fe7c2008f6721c24d98e
jbgraille <jbgraille@gmail.com>
2020-12-26 22:04:34 +0100

    string compression

--- ch-01-ArrayString/inscala/src/main/scala/arraystring/strcompress.scala
+++ ch-01-ArrayString/inscala/src/main/scala/arraystring/strcompress.scala
@@ -3,23 +3,40 @@ import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.Map
 
 object ArrayString {
-  def stringcompression(input: String): Map[Char,Int] = {
-      var len : Int = input.length()
-      var dic : scala.collection.mutable.Map[Char,Int] = Map()
-      var output : ListBuffer[String] = new ListBuffer[String]()
-      for (i <- input) {
-        if (dic.contains(i)) {
-          dic(i) += 1
-        } else {
-          dic += (i -> 1)
-        }
+  def stringcompression(input: String): String = {
+    var len : Int = input.length
+    var list : ListBuffer[String] = new ListBuffer[String]()
+    var dic : scala.collection.mutable.Map[String,Int] = Map()
+    var i : Int = 0
+    while (i < len) {
+      var s : String = input(i).toString
+      if (dic.contains(s)) {
+        dic(s) = dic(s) + 1
+      } else {
+        dic += (s -> 1)
       }
-      return dic
+      if (i < (len - 1)) {
+        if (input(i) != input(i+1)) {
+          list += s.concat(dic(s).toString)
+          dic.remove(s)
+        }
+      } else {
+        list += s.concat(dic(s).toString)
+      }
+      i = i + 1
+    }
+    var ans : String = list.mkString("")
+    if (ans.length > len) {
+      ans = input
+    }
+    return ans
   }
 
   def main(args: Array[String]) {
-    val myList : List[String] = List("aabcccccaaa","abc","AACTTTTUV")
-    println(stringcompression(myList(0)))
+    val myList : List[String] = List("aabcccccaaa","abc","AAAACTTTTUV")
+    val resList = myList.map(stringcompression)
+    println(myList)
+    println(resList)
    }
 
 }
