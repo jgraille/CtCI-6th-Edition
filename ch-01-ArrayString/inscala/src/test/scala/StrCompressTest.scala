e8e5d6c073e485b4e7bdf3e43deaefd88c695f82
jbgraille <jbgraille@gmail.com>
2020-12-29 21:46:32 +0100

    adding the empty case in test

--- ch-01-ArrayString/inscala/src/test/scala/StrCompressTest.scala
+++ ch-01-ArrayString/inscala/src/test/scala/StrCompressTest.scala
@@ -3,5 +3,6 @@ class StrCompressTest extends org.scalatest.funsuite.AnyFunSuite {
     assert(StrCompress.run("aabcccccaaa") === "a2b1c5a3")
     assert(StrCompress.run("abc") === "abc")
     assert(StrCompress.run("AAAACTTTTUV") === "A4C1T4U1V1")
+    assert(StrCompress.run("") === "")
   }
 }
