ca114ed9ba50a106ce972d84a289aab07276b7d9
jbgraille <jbgraille@gmail.com>
2021-04-25 13:19:03 +0200

    functionnal in scala

---
+++ FunctionalProgrammingPrinciples/week1/src/main/scala/recfun/RecFun.scala
@@ -0,0 +1,53 @@
+package recfun
+
+import scala.collection.mutable.ArrayBuffer
+
+object RecFun extends RecFunInterface {
+
+  def main(args: Array[String]): Unit = {
+    /*
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(s"${pascal(col, row)} ")
+    }
+    */
+    val res = balance("this is awesome)".toList)
+    println(res)
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+    else {
+      pascal(c-1,r-1) + pascal(c,r-1)
+    }
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    var c = chars
+    if (!c.isEmpty) {
+      if (c.head == ')') false
+      while (!c.tail.isEmpty) {
+        if (c.head == '(') {
+          if (c.tail.find(_ == ')') == None) false
+        }
+        if (c.last == ')') false
+        c = c.drop(1)
+      }
+      true
+    } else {
+      false
+    }
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
