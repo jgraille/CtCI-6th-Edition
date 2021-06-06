78e4804094107e99ad1ba438bd5825768292d2b2
jbgraille <jbgraille@gmail.com>
2021-06-06 12:57:20 +0200

    test

--- FunctionalProgrammingPrinciples/week1/src/main/scala/recfun/RecFun.scala
+++ FunctionalProgrammingPrinciples/week1/src/main/scala/recfun/RecFun.scala
@@ -15,8 +15,8 @@ object RecFun extends RecFunInterface {
     val res = balance("toto".toList)
     println(res)
     */
-    println("10, List(2,5)",changer(10,List(1,2,5),0))
-    println("4, List(2,4)",changer(4,List(1,2,4),0))
+    println(changer1(4,List(1,2),0))
+    println(changer1(300,List(5,10,20,50,100,200,500),0))
   }
 
   /**
@@ -56,31 +56,59 @@ object RecFun extends RecFunInterface {
   def changer(money: Int, coins: List[Int], c: Int): Int = {
     if (!coins.isEmpty) {
       if (floorMod(money,coins.head) == 0) {
-        return changer(money,coins.tail,c+1)
+        changer(money,coins.tail,c+1)
       } else {
-        val resModulo = floorMod(money,coins.head)
-        if (floorMod(money,resModulo + coins.head) == 0) {
-          return changer(money,coins.tail,c+1)
+        if (coins.contains(floorMod(money,coins.head + coins.tail.head)) && !coins.tail.isEmpty) {
+          changer(money,coins,c+1)
+        }
+        if (floorMod(money,floorMod(money,coins.head) + coins.head) == 0) {
+          changer(money,coins.tail,c+1)
         } else {
-          if (coins.contains(floorMod(money,resModulo + coins.head))) {
-            return changer(money,coins.tail,c+1)
+          if (coins.contains(floorMod(money,floorMod(money,coins.head) + coins.head))) {
+            changer(money,coins.tail,c+1)
           } else {
-            return changer(money,coins.tail,c)
+            changer(money,coins.tail,c)
           }
         }
-        return changer(money,coins.tail,c)
+        changer(money,coins.tail,c)
       }
     } else {
-      return c
+      c
+    }
+  }
+  def changer1(money: Int, coins: List[Int], c: Int): Int = {
+    if (coins.isEmpty || money <= 0) {
+      c
+    } else {
+      if (money < coins.head) { 
+        changer1(money,coins.tail,c)
+      } else if (floorMod(money,coins.head) == 0) {
+        if (!coins.tail.isEmpty){
+          if (coins.contains(floorMod(money,coins.head + coins.tail.head))) {
+            changer1(money,coins.tail,c+2)
+          } else {
+            changer1(money,coins.tail,c+1)
+          }
+        } else {
+          c + 1
+        }
+      } else if (floorMod(money,floorMod(money,coins.head) + coins.head) == 0) {
+        changer1(money,coins.tail,c+1)
+      }
+      else if (coins.contains(floorMod(money,floorMod(money,coins.head) + coins.head))) {
+        changer1(money,coins.tail,c+1)
+      } else {
+        changer1(money,coins.tail,c)
+      }
     }
   }
 
   def countChange(money: Int, coins: List[Int]): Int = {
     if (money == 0) {
-      return 0
+      0
     } else {
       if (coins.isEmpty) {
-        return 0
+        0
       } else {
         changer(money,coins,0)
       }
@@ -88,3 +116,4 @@ object RecFun extends RecFunInterface {
     
   }
 }
+
